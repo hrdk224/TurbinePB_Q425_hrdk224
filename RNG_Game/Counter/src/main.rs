cee9281b83f4af7914e7844e067ea29da185d3e6
hrdk224 <hardikkille07@gmail.com>
2025-10-24 09:22:48 +0000

    Counter update

--- RNG_Game/Counter/src/main.rs
+++ RNG_Game/Counter/src/main.rs
@@ -1,11 +1,39 @@
+use std::io;
 fn main() {
-    let mut count = 0;
-    loop {
-        println!("{count}");
-        count += 1;
+    let mut num = String::new();
+    println!("Welcome to counter!\n Enter a number: ");
+    io::stdin().read_line(&mut num).expect("failed to readline");
+    let mut num: i32 = num.trim().parse().expect("Please type a number");
 
-        if count == 100 {
-            break;
+    println!("What operation do you want to perform: \n + --> Increment \n - --> Decrement");
+
+    // if op == "+" {
+    //     num = num + 1;
+    //     println!("incremented output:{}", num);
+    // }
+
+    // if op == "-" {
+    //     num = num - 1;
+    //     println!("decremented output:{}", num);
+    // }
+    loop {
+        let mut op = String::new();
+        io::stdin().read_line(&mut op).expect("failed to readline");
+        let op: &str = op.trim();
+        match op {
+            "+" => {
+                num = num + 1;
+                println!("incremented output:{}", num);
+                break;
+            }
+            "-" => {
+                num = num - 1;
+                println!("decremented output:{}", num);
+                break;
+            }
+            _ => {
+                println!("input either + or -");
+            }
         }
     }
 }
